0c9b0dd9845cc0a6b4ad6da8fc647edee25a6154
Matias Bidart <matiasezequielbidart@gmail.com>
2022-07-08 13:54:48 -0300

    updating-new-files

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
     <div className='align-cntr flx-dr-clmn mrg-tp-3rem'>
         <h1>Error 404 / Path Not Found 😐</h1>
         <h3 className="errortitle">Sorry, this path may not exist...</h3>
-        <img className='rickrolled' src='assets\img\rickrolled.gif' alt='rickrolled'/>
+        <img className='rickrolled' src='https://c.tenor.com/zuWiNsNjKPYAAAAC/rick-astley-together-forever.gif' alt='rickrolled'/>
         <h2>But never Give You Up!</h2>
     </div>
   )
